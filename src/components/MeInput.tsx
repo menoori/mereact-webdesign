7ee6604f745db460592ea83d4fab49006fcf5475
menoori <49369613+menoori@users.noreply.github.com>
2022-11-28 00:23:30 +0100

    me-input

--- src/components/MeInput.tsx
+++ src/components/MeInput.tsx
@@ -1,11 +1,4 @@
-// REACT
 import React, { useState } from "react";
-
-// NPM INSTALLS
-
-/*
-  Information about the MeButton Component
-*/
 
 type HEX = `#${string}`;
 type TRANSITIONHEX = {
@@ -14,13 +7,13 @@ type TRANSITIONHEX = {
   firstColor: HEX;
   secondColor: HEX;
 };
-interface MeButtonProps {
-  callback?: () => void;
+
+interface MeInputProps {
+  type?: React.HTMLInputTypeAttribute | undefined;
   disabled?: boolean;
-  size?: "small" | "medium" | "large";
-  icon?: string;
-  text?: string;
-  transitionColor?:
+  placeholder?: string;
+  required?: boolean;
+  color?:
     | "OuterSpaceCrayola"
     | "InverseOuterSpaceCrayola"
     | "SpaceCadet"
@@ -30,63 +23,13 @@ interface MeButtonProps {
     | "EnglishViolet"
     | "InverseEnglishViolet"
     | TRANSITIONHEX;
-  transitionY?: boolean;
 }
-export default function MeButton(props: MeButtonProps) {
+export default function MeInput(props: MeInputProps) {
   // ----- STATE -----
   const [buttonClick, setButtonClick] = useState(false);
   const [buttonHover, setButtonHover] = useState(false);
   const [buttonFocus, setButtonFocus] = useState(false);
-
-  // ----- USE HOOKS -----
-
   // ----- STYLING -----
-  const fontSize = (): string => {
-    return props.size === "small" ? "1.4rem" : "1.6rem";
-  };
-  const buttonHeight = (): string => {
-    return props.size === "small"
-      ? "3.2rem"
-      : props.size === "medium"
-      ? "4.4rem"
-      : "5.2rem";
-  };
-  const spanPaddingInline = (): string => {
-    if (!props.text) return "0";
-    if (!props.icon)
-      return props.size === "small"
-        ? "1.6rem"
-        : props.size === "medium"
-        ? "2.4rem"
-        : "3.2rem";
-    return props.size === "small"
-      ? ".4rem 1.6rem"
-      : props.size === "medium"
-      ? ".8rem 2.4rem"
-      : ".8rem 3.2rem";
-  };
-  const iconPaddingInline = (): string => {
-    if (!props.icon) return "0";
-    if (!props.text)
-      return props.size === "small"
-        ? ".8rem"
-        : props.size === "medium"
-        ? "1.2rem"
-        : "1.4rem";
-    return props.size === "small"
-      ? "1.2rem 0"
-      : props.size === "medium"
-      ? "2.0rem 0"
-      : "2.4rem 0";
-  };
-  const iconSize = (): string => {
-    return props.size === "small"
-      ? "1.6rem"
-      : props.size === "medium"
-      ? "2.0rem"
-      : "2.4rem";
-  };
-
   const handleColor = (): {
     backgroundImage: string;
     firstColor: HEX;
@@ -94,7 +37,7 @@ export default function MeButton(props: MeButtonProps) {
     borderColor: HEX;
   } => {
     const angle = 30;
-    switch (props.transitionColor) {
+    switch (props.color) {
       case "OuterSpaceCrayola":
         return {
           backgroundImage: `linear-gradient(${angle}deg,#F6F7F5 50%,#2E3739 50%)`,
@@ -152,12 +95,12 @@ export default function MeButton(props: MeButtonProps) {
           borderColor: "#4A3A50",
         };
       default:
-        if (props.transitionColor?.fromHEX) {
+        if (props.color?.fromHEX) {
           return {
-            backgroundImage: `linear-gradient(${angle}deg, ${props.transitionColor.fromHEX} 50%,${props.transitionColor.toHEX} 50%)`,
-            firstColor: props.transitionColor.firstColor,
-            secondColor: props.transitionColor.secondColor,
-            borderColor: props.transitionColor.fromHEX,
+            backgroundImage: `linear-gradient(${angle}deg, ${props.color.fromHEX} 50%,${props.color.toHEX} 50%)`,
+            firstColor: props.color.firstColor,
+            secondColor: props.color.secondColor,
+            borderColor: props.color.fromHEX,
           };
         }
         return {
@@ -169,15 +112,12 @@ export default function MeButton(props: MeButtonProps) {
     }
   };
 
-  const buttonStyle: React.CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  const inputStyle: React.CSSProperties = {
+    paddingInline: "1.6rem",
+    paddingBlock: "1.8rem",
 
     opacity: props.disabled ? 0.5 : 1,
-    height: buttonHeight(),
 
-    backgroundImage: handleColor()?.backgroundImage,
     borderRadius: ".8rem",
     color: props.disabled
       ? handleColor().firstColor
@@ -185,47 +125,34 @@ export default function MeButton(props: MeButtonProps) {
       ? handleColor()?.secondColor
       : handleColor()?.firstColor,
     border: `3px solid ${handleColor()?.borderColor}`,
-    boxShadow: props.disabled
-      ? ""
-      : buttonFocus
-      ? `inset 0 0 0 2px ${handleColor().firstColor}`
-      : "none",
     filter: props.disabled
       ? "grayscale(100%)"
       : buttonClick
       ? "brightness(.8)"
       : "",
+    boxShadow: props.disabled
+      ? ""
+      : buttonFocus
+      ? `inset 0 0 0 2px ${handleColor().firstColor}`
+      : "none",
+    backgroundImage: handleColor()?.backgroundImage,
     backgroundSize: "100vw",
     backgroundRepeat: "no-repeat",
     backgroundPosition: props.disabled ? "" : buttonHover ? "100%" : "0%",
     transition:
       "background 300ms ease-in-out, color 300ms ease-in-out, transform 300ms cubic-bezier(0.57, 0.21, 0.69, 3.25)",
 
-    fontSize: fontSize(),
-    fontWeight: "600",
+    fontSize: "1.6rem",
+    fontWeight: "400",
 
-    cursor: props.disabled ? "not-allowed" : "pointer",
-    transform: props.disabled
-      ? ""
-      : buttonHover && props.transitionY
-      ? "translateY(-0.5rem)"
-      : "translateY(0)",
+    cursor: props.disabled ? "not-allowed" : "text",
   };
-
-  const spanStyle: React.CSSProperties = {
-    paddingInline: spanPaddingInline(),
-  };
-
-  const iconStyle: React.CSSProperties = {
-    paddingInline: iconPaddingInline(),
-    fontSize: iconSize(),
-  };
-
   return (
-    <button
-      className="mereact-mebutton"
-      style={buttonStyle}
-      onClick={props.callback}
+    <input
+      type={props.type}
+      placeholder={props.placeholder}
+      style={inputStyle}
+      required={props.required}
       onTouchStart={() => setButtonClick(true)}
       onTouchEnd={() => setButtonClick(false)}
       onMouseDown={() => setButtonClick(true)}
@@ -235,9 +162,6 @@ export default function MeButton(props: MeButtonProps) {
       onFocus={() => setButtonFocus(true)}
       onBlur={() => setButtonFocus(false)}
       disabled={props.disabled}
-    >
-      <i style={iconStyle}>{props.icon}</i>
-      <span style={spanStyle}>{props.text}</span>
-    </button>
+    />
   );
 }
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import MeButton from '../components/MeButton'
+import MeInput from '../components/MeInput'
 
 export default function TestPage() {
     return (
         <div>
-            <MeButton transitionY text='my button' size='small' transitionColor={'AmaranthPurple'}/>
+            <MeButton disabled callback={()=>console.log("hej")} transitionY text='my button' size='small' transitionColor={'AmaranthPurple'}/>
             <MeButton transitionY text='my button' size='medium' transitionColor={'InverseOuterSpaceCrayola'}/>
             <MeButton transitionY text='my button' size='large' transitionColor={'SpaceCadet'}/>
+        <MeInput placeholder='password' color='AmaranthPurple'/>
+        <MeInput placeholder='password' color='InverseEnglishViolet' disabled/>
         </div>
     )
 }
